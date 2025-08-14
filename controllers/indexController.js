f13266d590268e36de9effcbfa0c884368ec80d5
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-08-14 11:26:14 -0400

    Getting started to add login/sign up functions

---
+++ controllers/indexController.js
@@ -0,0 +1,27 @@
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+const emptyErr = "is required"
+const lengthErr = "must be between 1 and 50 characters."
+const emailErr = "must be formatted like an email."
+const pwMatchErr = "Passwords do not match"
+
+const validateUser = [
+    body("fullname").trim()
+    .notEmpty().withMessage(`Full name ${emptyErr}`)
+    .isLength({ min: 1, max: 50 }).withMessage(`Full name ${lengthErr}`),
+    body("username").trim()
+    .isLength({ min: 1, max: 50 }).withMessage(`Full name ${lengthErr}`)
+    .isEmail().withMessage(`Email ${emailErr}`), 
+    body('password')
+    .notEmpty().withMessage(`Password ${emptyErr}`)
+    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+    body('confirmpassword')
+        .notEmpty().withMessage(`Confirm Password ${lengthErr}`)
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error(`${pwMatchErr}`);
+            }
+            return true;
+        }),
+]
---
+++ routes/indexRouter.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+const indexRouter = Router();
+const indexController = require("../controllers/indexController")
+const passport = require("passport");
+
+//home page
+indexRouter.get('/', indexController.generateIndex)
+
+//log in
+indexRouter.get('/log-in', (req, res) => res.render('log-in-form'))
+indexRouter.post(
+      "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+//log out (passport adds a logout function to the req object)
+indexRouter.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
