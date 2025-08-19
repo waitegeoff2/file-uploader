3e116c3332c9a5be3969985b2cd649991d90e262
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-08-19 14:59:00 -0400

    Fixed modals?

--- public/modal.js
+++ public/modal.js
@@ -1,28 +1,27 @@
 const newFolderBtn = document.querySelector('.new-folder-btn')
-const newFileBtn = document.querySelector('.new-file-btn')
-const fileModal = document.querySelector('.file-modal')
 const modal = document.querySelector('.modal')
-const fileClose = document.querySelector('.file-close-btn')
 const closeBtn = document.querySelector('.close-btn')
-
+// const newFileBtn = document.querySelector('.new-file-btn')
+// const fileModal = document.querySelector('.file-modal')
+// const fileClose = document.querySelector('.file-close-btn')
 
 newFolderBtn.addEventListener('click', () => {
     modal.style.display = 'block';
 })
 
-newFileBtn.addEventListener('click', () => {
-    fileModal.style.display = 'block';
-})
+// newFileBtn.addEventListener('click', () => {
+//     fileModal.style.display = 'block';
+// })
 
 closeBtn.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
 }
 
-fileClose.onclick = function() {
-    fileModal.style.display = "none";
-}
+// fileClose.onclick = function() {
+//     fileModal.style.display = "none";
+// }
 
-//click outside modal, it closes
+// click outside modal, it closes
 window.onclick = function(event) {
     if (event.target == (modal)) {
         modal.style.display = "none"
