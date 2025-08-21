c5358201324deb39eae9b462fc4271c26711268e
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-08-21 12:29:18 -0400

    Added styling

--- db/prisma.js
+++ db/prisma.js
@@ -3,20 +3,4 @@
 const { PrismaClient } = require('../generated/prisma')
 const prisma = new PrismaClient();
 
-// async function main() {
-//     const allUsers = await prisma.user.findMany()
-//   console.log(allUsers)
-// }
-
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
-
-// exporting the prisma client to be used elsewhere
 module.exports = prisma;
